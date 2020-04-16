01389937b853e487edfdcb9e48e57e7f868cefa5
Sam Pomerantz <sampom100@gmail.com>
2020-04-16 17:43:17 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ import yfinance as yf
 from GUI import onButton
 
 # Default ticker is Apple
-ticker = "AAPL"
+ticker = "SPY"
+DateArray = yf.Ticker(ticker).options
 
 
 def askForTicker():
     # Prompt the user for a new ticker
+    global ticker
     ticker = onButton()
     print("Ticker was: " + ticker)
 
@@ -14,9 +16,17 @@ def askForTicker():
 def getOptionsChain(inputString):
     # Retrieve the Options Chain Expiration dates from Yahoo Finance
     YFticker = yf.Ticker(inputString)
+    global DateArray
     DateArray = YFticker.options
+    print("Options Chain Recieved")
+
+
+def displayOptionsChain():
+    #print("Items: " + len(DateArray))
     print("\n".join(DateArray))
 
 
 askForTicker()
 getOptionsChain(ticker)
+
+displayOptionsChain()
